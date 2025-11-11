3cee202e877b5708274c5b6ac3660a9ef12bb587
epercar444 <epercar444@g.educaand.es>
2025-11-11 09:30:31 +0100

    clase 11/11

--- des_inter/Proyecto_PySide6/segundas_actividades/EvaPerez_QProgressBar.py
+++ des_inter/Proyecto_PySide6/segundas_actividades/EvaPerez_QProgressBar.py
@@ -1,7 +1,7 @@
-#Eva Pérez Carmona
+# Eva Pérez Carmona
 import sys
 from PySide6.QtCore import QTimer
-from PySide6.QtWidgets import QApplication, QMainWindow, QProgressBar,QTimer
+from PySide6.QtWidgets import QApplication, QMainWindow, QProgressBar
 
 class MainWindow(QMainWindow):
     def __init__(self):
@@ -10,34 +10,46 @@ class MainWindow(QMainWindow):
         self.progreso_actual = 0
 
         self.progreso = QProgressBar()
-        self.progreso.setRange(0,100)
+        self.progreso.setRange(0, 100)
 
         self.timer = QTimer()
         self.timer.timeout.connect(self.preguntar_usuario)
         self.timer.start(2000)
 
-
         self.setCentralWidget(self.progreso)
 
-    def preguntar_usuario(self) :
-        print ("===Control de progreso === 1 -> Aumentar progreso 2 -> Retroceder progreso 0 -> Salir")
-        opcion = int(input("Elige una opción: "))
-        #if opcion == 1 :
+    def preguntar_usuario(self):
+        print("=== Control de progreso ===")
+        print("1 -> Aumentar progreso")
+        print("2 -> Retroceder progreso")
+        print("0 -> Salir")
 
+        opcion = input("Elige una opción: ")
 
+        if opcion == "1":
+            self.cambiar_progreso("aumentar")
+        elif opcion == "2":
+            self.cambiar_progreso("disminuir")
+        elif opcion == "0":
+            print("Saliendo del programa...")
+            self.timer.stop()
+            QApplication.quit()
+        else:
+            print("Opción no válida.")
 
-    def cambiar_progreso(self,tipo) :
-        if tipo == "aumentar" :
-            self.progreso_actual = min(self.progreso_actual+20,100) #se asegura que aunque la suma pase de 100, devuelve 100
-        else :
-            self.progreso_actual = max(self.progreso_actual - 20,0)
-        self.progreso.setValue(self.progreso_actual) 
-        if self.progreso_actual == 100 :
+    def cambiar_progreso(self, tipo):
+        if tipo == "aumentar":
+            self.progreso_actual = min(self.progreso_actual + 20, 100)
+        else:
+            self.progreso_actual = max(self.progreso_actual - 20, 0)
+
+        self.progreso.setValue(self.progreso_actual)
+
+        if self.progreso_actual == 100:
             self.setWindowTitle("¡Tarea completada!")
-        else :
-            self.setWindowTitle("Progreso",self.progreso_actual,"%")
-            print("Progreso actual",self.progreso_actual,"%")
-    
+        else:
+            self.setWindowTitle(f"Progreso {self.progreso_actual}%")
+            print(f"Progreso actual: {self.progreso_actual}%")
 
 app = QApplication(sys.argv)
 window = MainWindow()
--- op/VisualStudio(Express)/03Express/01-express.js
+++ op/VisualStudio(Express)/03Express/01-express.js
@@ -6,11 +6,6 @@ app.use(express.static(__dirname + '/public'));
 app.get('/', (req, res) => {
   res.send('Ya somos unos cracks en Node+Express!')
 })
-
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
-
 app.get('/contacto.html',(req,res) => {
   res.send("Estás en contacto")
   console.log(__dirname)
@@ -20,6 +15,12 @@ app.get('/aboutUs',(req,res) => {
   res.send("Estás en aboutUs")
     console.log(__dirname)
 })
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
+
+
 
 
 //error instalando nodemon
