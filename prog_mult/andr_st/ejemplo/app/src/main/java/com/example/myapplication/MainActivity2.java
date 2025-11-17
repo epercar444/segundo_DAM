d2b2a7dcb193d450e63be27ddc94130a1202219c
epercar444 <epercar444@g.educaand.es>
2025-11-17 13:54:37 +0100

    clase 17/11

--- prog_mult/andr_st/ejemplo/app/src/main/java/com/example/myapplication/MainActivity2.java
+++ prog_mult/andr_st/ejemplo/app/src/main/java/com/example/myapplication/MainActivity2.java
@@ -1,22 +1,36 @@
 package com.example.myapplication;
 
+import android.graphics.Typeface;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity2 extends AppCompatActivity {
-  /*  @Override
+    @Override
     protected void onCreate(Bundle savedIns) {
         super.onCreate(savedIns);
         setContentView(R.layout.activity_actividad1);
         Bundle b = new Bundle(getIntent().getExtras());
         Log.i("Datos:",b.getString("Surname"));
-    }*/
-  @Override
+
+        TextView miTexto = (TextView) findViewById(R.id.texto);
+        miTexto.setText(("Nuevo texto para mostrar"));
+        Animation miAnimacion = AnimationUtils.loadAnimation(this, R.anim.animacion);
+        miAnimacion.setRepeatMode(Animation.RESTART);
+        miAnimacion.setRepeatCount(20);
+        miTexto.startAnimation(miAnimacion);
+        //miTexto.setTextColor(getResources().getColor(R.color.principal));
+       // Typeface miFuente = Typeface.createFromAsset((getAssets()),"fonts/karla.ttf");
+        //miTexto.setTypeface(miFuente);
+    }
+  /*@Override
   protected void onCreate(Bundle savedIns) {
       super.onCreate(savedIns);
       setContentView(R.layout.prueba1_linearlayout);
-  }
+  }*/
 }
 
